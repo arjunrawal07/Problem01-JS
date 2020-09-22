b51e067db18fac67bd118585e76cd5fa478f1b2d
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-09-22 12:11:29 -0400

    WuTang solution

--- WuTangForeverBuzz.js
+++ WuTangForeverBuzz.js
@@ -1,9 +1,19 @@
 class WuTangForeverBuzz {
-    wuTangClan(){
-        let output ="";
-
-        return output;
+  wuTangClan() {
+    let output = "";
+    for (let i = 1; i <= 15; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        output += "WuTangForever\n";
+      } else if (i % 3 === 0) {
+        output += "Wu\n";
+      } else if (i % 5 === 0) {
+        output += "Tang\n";
+      } else {
+        output += i + "\n";
+      }
     }
+    return output;
+  }
 }
 
 module.exports = WuTangForeverBuzz;
